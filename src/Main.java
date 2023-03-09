d5c0967283e86ca2e49f142e6a0b7b7f2a72cbbe
Marlon <marlon.ortiz19@gmail.com>
2023-03-09 00:12:21 -0300

    Update Main.java
    vetor de double para Double

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        double[] vector = new double[n];
+        Double[] vector = new Double[n];
      
         for (int i=0; i<n; i++) {
             System.out.println("Digiite um valor: ");
